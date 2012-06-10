17ee9def993ef730d98452457b47a8f80c798a69
Saurabh Sood <saurabhsood91@gmail.com>
2012-06-10 15:28:12 +0530

    implementation of package installation in a function

--- src/backend/package.cpp
+++ src/backend/package.cpp
@@ -46,3 +46,43 @@ OCI::Package::setDescription( const QString& description )
 {
 	m_description = description;
 }
+
+void
+OCI::Package::installPackage( const zypp::ZYpp::Ptr& zypp_pointer )
+{
+    bool rres;
+    m_status = false;
+    zypp::ui::Selectable::Ptr s = zypp::ui::Selectable::get( this->name().toStdString() );
+    if ( !s ) {
+        qDebug()<<"Not initialised";
+        return;
+    }
+    zypp::PoolItem p = s->highestAvailableVersionObj();
+    s->setCandidate( p );
+    zypp::ResPool pool = zypp::ResPool::instance();
+    p.status().setToBeInstalled( zypp::ResStatus::USER );
+    rres = zypp::ResPool::instance().resolver().resolvePool();
+    if ( !rres ) {
+        qDebug()<<"Failed to Resolve Pool";
+        return;
+    }else {
+        qDebug()<<"Pool Resolved";
+    }
+    zypp::ZYppCommitPolicy policy;
+    policy.restrictToMedia( 0 );
+    policy.downloadMode( zypp::DownloadInHeaps );
+    policy.syncPoolAfterCommit( true );
+    zypp::ZYppCommitResult result = zypp_pointer->commit( policy );
+    if ( result.allDone() ) {
+        qDebug()<<"Installation Completed";
+        m_status = true;
+    }else {
+        qDebug()<<"Installation Failed";
+    }
+}
+
+bool
+OCI::Package::isInstalled()
+{
+    return m_status;
+}
--- src/backend/package.h
+++ src/backend/package.h
@@ -2,6 +2,13 @@
 #define PACKAGE_H
 
 #include <QString>
+#include <zypp/ZYppFactory.h>
+#include <zypp/ZYppCommit.h>
+#include <zypp/misc/DefaultLoadSystem.h>
+#include <zypp/ResPool.h>
+#include <zypp/ResStatus.h>
+#include <zypp/ResFilters.h>
+#include <QDebug>
 
 namespace OCI{
 class Package
@@ -15,10 +22,13 @@ class Package
 		void setName( const QString& name );
 		void setSummary( const QString& summary );
 		void setDescription( const QString& description );
+                void installPackage( const zypp::ZYpp::Ptr& zypp_pointer );
+                bool isInstalled();
 	private:
 		QString m_name;
 		QString m_summary;
 		QString m_description;
+                bool m_status;
 };
 }
 
