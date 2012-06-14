6d0faa910fe8b7a3f09d43dad86004f4deefd99f
Saurabh Sood <saurabhsood91@gmail.com>
2012-06-15 01:36:06 +0530

    added check for no YMP file being passed as argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <QApplication>
+#include <QDebug>
 #include "mainwindow.h"
 
 int main( int argc, char *argv[] )
 {
 	QApplication app( argc, argv );
+	if( argc == 1){
+		qDebug() << "No Valid File Passed";
+		return 0;
+	}
 	MainWindow *win = new MainWindow( QString( argv[1] ) );
 	win->show();
 	return app.exec();
