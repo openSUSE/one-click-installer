1edee5696d263653f982b8c72d2fe62fbfc2e1bf
Saurabh Sood <saurabhsood91@gmail.com>
2012-06-18 19:43:50 +0530

    added function descriptions for the Backend

--- src/PackageBackend.h
+++ src/PackageBackend.h
@@ -7,11 +7,34 @@
 class PackageBackend
 {
     public:
+    /**
+     	Adds the repository required to install the selected packages. This will be added to the system as installation and update repository
+     */
     virtual void addRepository( const QUrl& url  );
+
+    /**
+    	Add the package to be installed
+     */
     virtual void addPackage( const QString& package);
+
+    /**
+    	Resolve Conflicts that may arise out of selecting packages
+     */
     virtual bool resolveConflicts();
+
+    /**
+     	Return the list of packages selected for installation
+     */
     virtual QList< QString > packages();
+
+    /**
+    	Return the list of repositories to be added
+     */
     virtual QList< QUrl > repositories(); 
+
+    /**
+    	Carry out the installation and adding of repositories
+     */
     virtual void install() = 0;
 
     private:
