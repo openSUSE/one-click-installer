13932ee738e79cc3712cb3537fbd236bacce51ed
Saurabh Sood <saurabhsood91@gmail.com>
2012-06-22 11:13:01 +0530

    improved UI of settings dialog

--- src/settings.cpp
+++ src/settings.cpp
@@ -4,9 +4,9 @@ Settings::Settings( QObject *parent )
 {
 	//Create Layouts
 	QVBoxLayout *mainLayout = new QVBoxLayout;
-
+        QHBoxLayout *closeLayout = new QHBoxLayout;
 	//Create Interface Elements
-	m_repos = new QLabel( "Repository Sources" );
+        m_repos = new QLabel( "<b>Repository Sources</b>" );
 	m_subscribe = new QCheckBox( "Subscribe to new Repository Sources automatically", this );
 	m_trust = new QCheckBox( "Trust and import repository public keys by default", this );
 	m_close = new QPushButton( "Close" );
@@ -15,13 +15,13 @@ Settings::Settings( QObject *parent )
 	mainLayout->addWidget( m_repos );
 	mainLayout->addWidget( m_subscribe );
 	mainLayout->addWidget( m_trust );
-	mainLayout->addWidget( m_close );
-	
+        closeLayout->addSpacing(150);
+        closeLayout->addWidget( m_close );
+        mainLayout->addLayout( closeLayout );
 	setLayout( mainLayout );
         setWindowTitle( "One Click Install Settings" );
-	show();
-
         QObject::connect( m_close, SIGNAL(clicked()), this, SLOT( saveSettingsAndClose()) );
+        show();
 }
 
 void
