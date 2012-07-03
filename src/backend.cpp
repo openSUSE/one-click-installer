2f2f67888aa5d5c6b01fda392ed187ccdf87d46b
Saurabh Sood <saurabhsood91@gmail.com>
2012-07-03 17:34:18 +0530

    code to check for existing repositories now working

--- src/backend.cpp
+++ src/backend.cpp
@@ -14,6 +14,7 @@ Backend::Backend()
 
 void Backend::addRepositories()
 {
+    qDebug() << "add repos called";
     foreach( QUrl iter, repositories() ){
         zypp::RepoInfo repoInfo;
         std::string url = iter.toString().toStdString();
@@ -23,6 +24,8 @@ void Backend::addRepositories()
 
         if( !exists( url ) )
             m_manager->addRepository( repoInfo );
+        else
+            qDebug() << "Repository Exists";
         m_manager->buildCache( repoInfo );
         m_manager->loadFromCache( repoInfo );
     }
@@ -30,9 +33,11 @@ void Backend::addRepositories()
 
 bool Backend::exists( std::string repo )
 {
+    std::cout << "Parameter is " << repo << std::endl;
     std::list< zypp::RepoInfo > repoList = std::list< zypp::RepoInfo >( m_manager->repoBegin(), m_manager->repoEnd() );
     for( std::list< zypp::RepoInfo >::iterator it = repoList.begin(); it != repoList.end(); it++ ){
-        if( repo == it->url() )
+        //std::cout <<std::endl<<"Repo URL is " << it->url().asString();
+        if( repo.compare( it->url().asString() ) == 0 )
                 return true;
     }
     return false;
