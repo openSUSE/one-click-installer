19e52b9c4e7c8edd67c101dc9e3301b09045355d
Saurabh Sood <saurabhsood91@gmail.com>
2012-08-04 02:43:19 +0530

    used mktemp to create temporary directory

--- generate-tarball.sh
+++ generate-tarball.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 
-version=$( egrep -o '([0-9]+\.)+[0-9]+' VERSION.txt )
+version=$( egrep -o '([0-9]+\.)+[0-9]+' version.h )
 foldername=oneclickinstaller-$version
 echo $version
 echo $foldername
 
-mkdir -p /tmp/$foldername/src/res
+tempfolder=$( mktemp -d /tmp/tmp.XXXX )
+srcfolder=$( pwd )
+echo $tempfolder
 
-cp src/*.h src/*.cpp src/CMakeLists.txt src/oci.desktop /tmp/$foldername/src
-cp src/res/oneclickinstall.png /tmp/$foldername/src/res
+mkdir -p $tempfolder/$foldername/src/res
+
+cp src/*.h src/*.cpp src/CMakeLists.txt src/oci.desktop $tempfolder/$foldername/src
+cp src/res/oneclickinstall.png $tempfolder/$foldername/src/res
 
 branch=$( git branch | grep -o tags )
 echo $branch
@@ -16,7 +20,7 @@ echo $branch
 git tag -a v$version -m 'version '.$version
 git push --tags
 
-cd /tmp
+cd $tempfolder
 tar -cvf oneclickinstaller-$version.tar.gz oneclickinstaller-$version
-
-rm -rf /tmp/$foldername
+cp oneclickinstaller-$version.tar.gz $srcfolder
+rm -rf $tempfolder/$foldername
