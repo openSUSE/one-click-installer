bb5ee66a8be1a5d537e09522fcd4c7962d18393a
Saurabh Sood <saurabhsood91@gmail.com>
2012-08-04 01:40:29 +0530

    changed regex and handled tags

--- generate-tarball.sh
+++ generate-tarball.sh
@@ -1,15 +1,26 @@
 #! /bin/bash
 
-version=$( grep -o [0-9].[0-9] VERSION.txt )
+version=$( egrep -o '([0-9]+\.)+[0-9]+' VERSION.txt )
 foldername=oneclickinstaller-$version
 echo $version
 echo $foldername
 
-rm -rf $foldername
+mkdir -p /tmp/$foldername/src/res
 
-mkdir -p $foldername/src/res
+cp src/*.h src/*.cpp src/CMakeLists.txt src/oci.desktop /tmp/$foldername/src
+cp src/res/oneclickinstall.png /tmp/$foldername/src/res
 
-cp src/*.h src/*.cpp src/CMakeLists.txt src/oci.desktop $foldername/src
-cp src/res/oneclickinstall.png $foldername/src/res
+branch=$( git branch | grep -o tags )
+echo $branch
 
+if [ $branch == "tags" ]; then
+git tag -a v$version -m 'version '.$version
+git push -u origin
+else
+	echo "Not in tags. Not pushed"
+fi
+
+cd /tmp
 tar -cvf oneclickinstaller-$version.tar.gz oneclickinstaller-$version
+
+rm -rf /tmp/$foldername
