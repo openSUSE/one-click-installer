7cabb2a0453eadcad3d9853aee33bbd0f863cc94
Shalom <shalomray7@gmail.com>
2016-08-17 02:07:48 +0530

    Instantiated static objects for RpmCallbacks, SourceCallbacks, and MediaCallbacks in interfacemain.cpp

--- src/interfacemain.cpp
+++ src/interfacemain.cpp
@@ -32,7 +32,7 @@
 #include <QStringList>
 #include "backend.h"
 #include "utils.h"
-
+#include "media.h"
 int main( int argc, char *argv[] )
 {
     QCoreApplication app ( argc, argv );
@@ -40,6 +40,21 @@ int main( int argc, char *argv[] )
     if ( argc < 2 ) {
 	qDebug() << "Usage: ./oneclickhelper <File Path>";
 	return 1;
+    }
+ 
+    // Progress callbacks
+    try {
+	static RpmCallbacks rpmCallbacks;
+	static SourceCallbacks sourceCallbacks;
+	static MediaCallbacks mediaCallbacks;
+    }
+    catch ( const Exception& e )  {
+	qDebug() << "Failed to initialize OCI callbacks.";
+	exit( 1 );
+    }
+    catch (...) {
+	qDebug() << "Failed to initialize OCI callbacks.";
+	exit( 1 );
     }
     
     // Reset RepoManager. This step is necessary to run the manager with root privileges
